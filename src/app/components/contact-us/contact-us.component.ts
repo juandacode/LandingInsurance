9d3e8a492a29c25a35c52ca21327a5a7172373cd
juandacode <juandacode@gmail.com>
2022-07-25 16:34:34 -0500

    ContactUs Component Added

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent implements OnInit {
-  contact: string = 'Contact Us';
+  contact: string = 'Sea parte de nuestra familia';
   contactUsImage: string = '../../../../assets/images/contact-us-image.png';
   contactUsCalendar: string = '../../../../assets/icons/calendar.svg';
   constructor() { }
